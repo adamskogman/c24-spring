705f017df572d99486bd2c4965815621659ad660
adamskogman <askogman@vmware.com>
2011-07-06 19:36:06 +0000

    Model now doubles as a SourceFactory, so that the default source for a model can be used w/o configuration.
    git-svn-id: https://c24tech.jira.com/svn/SPR/trunk@17598 e6edf6fb-f266-4316-afb4-e53d95876a76

--- io-spring-core/src/main/java/biz/c24/io/spring/util/C24Model.java
+++ io-spring-core/src/main/java/biz/c24/io/spring/util/C24Model.java
@@ -1,5 +1,7 @@
 package biz.c24.io.spring.util;
 
+import java.io.InputStream;
+import java.io.Reader;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
@@ -10,13 +12,16 @@ import org.springframework.util.Assert;
 import biz.c24.io.api.data.ComplexDataObject;
 import biz.c24.io.api.data.DocumentRoot;
 import biz.c24.io.api.data.Element;
+import biz.c24.io.api.presentation.Source;
+import biz.c24.io.spring.source.SourceFactory;
 
 /**
- * Simple value object to lookup supported types and {@link Element}s of a {@link C24Model}.
- *
+ * Simple value object to lookup supported types and {@link Element}s of a
+ * {@link C24Model}.
+ * 
  * @author Oliver Gierke
  */
-public class C24Model {
+public class C24Model implements SourceFactory {
 
 	private final Map<Class<? extends ComplexDataObject>, Element> map = new HashMap<Class<? extends ComplexDataObject>, Element>();
 	private final Element rootElement;
@@ -32,19 +37,28 @@ public class C24Model {
 		Assert.notNull(element);
 		this.rootElement = element;
 
-		Class<? extends ComplexDataObject> type = element.getType().getValidObjectClass();
+		Class<? extends ComplexDataObject> type = element.getType()
+				.getValidObjectClass();
 
 		if (!DocumentRoot.class.isAssignableFrom(type)) {
+		
+			// If the model wraps something which is not a document root class,
+			// then just use it as-is
 			map.put(type, element);
-			return;
+
+		} else {
+			
+			// For document root classes, inspect the child elements.
+			
+			DocumentRoot root = (DocumentRoot) BeanUtils.instantiateClass(type);
+
+			for (int i = 0; i < root.getElementDeclCount(); i++) {
+				Element declaredElement = root.getElementDecl(i);
+				map.put(declaredElement.getType().getValidObjectClass(), element);
+			}
+			
 		}
 
-		DocumentRoot root = (DocumentRoot) BeanUtils.instantiateClass(type);
-
-		for (int i=0; i < root.getElementDeclCount(); i++) {
-			Element declaredElement = root.getElementDecl(i);
-			map.put(declaredElement.getType().getValidObjectClass(), element);
-		}
 	}
 
 	/**
@@ -74,4 +88,25 @@ public class C24Model {
 	public Set<Class<? extends ComplexDataObject>> getSupportedTypes() {
 		return map.keySet();
 	}
+
+	/**
+	 * @return A cloned instance of the models default source.
+	 */
+	public Source getRootElementSource() {
+		// A clone is returned by source(), no need to create a new one.
+		return getRootElement().getModel().source();
+	}
+
+	public Source getSource(Reader reader) {
+		Source source = getRootElementSource();
+		source.setReader(reader);
+		return source;
+	}
+
+	public Source getSource(InputStream stream) {
+		Source source = getRootElementSource();
+		source.setInputStream(stream);
+		return source;
+	}
+
 }
